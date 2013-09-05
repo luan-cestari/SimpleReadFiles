a87b1715839260eea07c6f9c25701a54177db33e
Luan Cestari <luan.cestari@gmail.com>
2013-09-04 23:24:41 -0300

    Draft of the solution

--- src/main/java/com/ourdailycodes/simplereadfiles/main/SRFMain.java
+++ src/main/java/com/ourdailycodes/simplereadfiles/main/SRFMain.java
@@ -1,31 +1,52 @@
 package com.ourdailycodes.simplereadfiles.main;
 
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.io.FileInputStream;
+import java.nio.channels.FileChannel;
+import java.nio.ByteBuffer;
+
 public class SRFMain {
 	public static void main(String[] args) throws Exception {
 		System.out.println("!!!BEGIN!!!");
 		String encoding = System.getProperty("file.encoding");
-		String threadpoolmax = System.getProperty("threadpoolmax");
+		String threadpoolmaxStr = System.getProperty("threadpoolmax");
+		String queuesizeStr = System.getProperty("queuesize");
+		int tpm = threadpoolmaxStr == null ? 1 : Integer.parseInt(threadpoolmaxStr);
+		int queueSize = queuesizeStr == null ? 1 : Integer.parseInt(queuesizeStr);
 
-		// TODO Create the THreadPool , reading all the files (and waiting for available thread), put all files read into the memory and process them with the business rules
-		for(String s : args){
-			System.out.println(s);
+		ThreadPoolExecutor e = new ThreadPoolExecutor(tpm, tpm, 3, TimeUnit.MINUTES, new ArrayBlockingQueue<Runnable>(queueSize, true), new ThreadPoolExecutor.CallerRunsPolicy());
+
+		for(final String s : args){
+			e.submit(new Runnable() {
+				@Override
+				public void run() {
+					try {
+						long startTime = System.currentTimeMillis();
+						FileInputStream f = new FileInputStream( s );
+						FileChannel ch = f.getChannel( );
+						byte[] barray = new byte[262144];
+						ByteBuffer bb = ByteBuffer.wrap( barray );
+						long checkSum = 0L;
+						int nRead;
+						while ( (nRead=ch.read( bb )) != -1 )
+						{
+							for ( int i=0; i<nRead; i++ )
+								checkSum += barray[i];
+							bb.clear( );
+						}
+						long stopTime = System.currentTimeMillis();
+						long elapsedTime = stopTime - startTime;
+						System.out.println(s+" : "+elapsedTime);
+					} catch (Exception ex) {
+					} finally {
+					}
+				}
+			});
 		}
+		e.shutdown();
+		e.awaitTermination(1, TimeUnit.MINUTES);
 		System.out.println("!!!END!!!");
 	}
-	/*
-	{
-		FileInputStream f = new FileInputStream( name );
-		FileChannel ch = f.getChannel( );
-		byte[] barray = new byte[262144];
-		ByteBuffer bb = ByteBuffer.wrap( barray );
-		long checkSum = 0L;
-		int nRead;
-		while ( (nRead=ch.read( bb )) != -1 )
-		{
-			    for ( int i=0; i<nRead; i++ )
-					        checkSum += barray[i];
-				    bb.clear( );
-		}
-	}
-	*/
 }
